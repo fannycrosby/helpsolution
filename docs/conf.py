724a34109a3092a187ee9dace0d9e874ee859c89
fannycrosby <contacts@yoursuppfix.link>
2025-06-17 20:13:37 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Sbcglobal Help Center'
+project = 'Sbcglobal Help Site'
 author = 'Support Team'
 release = '1.0'
 copyright = '2025'
